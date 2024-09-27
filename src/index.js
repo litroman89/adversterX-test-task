e24cb02426afca0c089f8a11e18744cc0a9e22eb
Roman Litiahin <roman.litiahin@gmail.com>
2024-09-27 22:05:01 +0300

    imoprted css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { userApi } from "./api/UserApi.js";
 import UserList from "./components/UserList.js";
 
+import "./css/main.css";
+
 async function init() {
   const users = await userApi.getUsers();
   const userList = new UserList(users);
